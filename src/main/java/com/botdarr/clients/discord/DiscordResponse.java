7c9c92ee71ddadff83fbdb2e3b3cca6353f6a622
Marjhonie <157345383+itsauzor@users.noreply.github.com>
2024-05-23 03:40:53 +0800

    Update DiscordResponse.java

--- src/main/java/com/botdarr/clients/discord/DiscordResponse.java
+++ src/main/java/com/botdarr/clients/discord/DiscordResponse.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.interactions.components.Component;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class DiscordResponse implements ChatClientResponse {
   public DiscordResponse(MessageEmbed message) {
     this.message = message;
